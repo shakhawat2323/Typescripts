3dcc53b97c25312fa2f7b82e051a22ead76e44bf
shakhawat2323 <shakhawathossain208@gmail.com>
2025-05-25 14:55:26 +0600

    basic typescript  type

---
+++ Module-2 install typescript and fast node version manager/modul2/src/index.ts
@@ -0,0 +1,25 @@
+const fast: string = "next level web developer";
+console.log(fast);
+
+const data: number[] = [2, 4, 53, 2, 14];
+data.push(3, 2, 66);
+console.log(data);
+
+const datas: string[] = ["shakhawat", "mona sona"];
+datas.push("insert data");
+
+console.log(datas);
+
+// 1. ami judi tuple korte chai tahole korte pari
+
+const tuples: [number, string, string, boolean] = [
+  1,
+  "diploma engineer",
+  "5 semeinser",
+  false,
+];
+
+console.log(tuples);
+
+const numbers: [number, number] = [23, 34];
+console.log(numbers);
